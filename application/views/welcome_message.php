f23db3dac3665f5d165296ee6cc1f2a71a4bfa75
Kgotso Maduna <kgmaduna01@gmail.com>
2021-07-09 15:34:44 +0200

    portfolio website

---
+++ application/views/welcome_message.php
@@ -0,0 +1,103 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to my webPage</title>
+
+	<link rel="stylesheet" href ="<?php echo base_url();?>Assets/Styles/Styles.css">
+</head>
+
+<body background="Assets/Images/bImage.jpg" >
+
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#About">About</a>
+  <a href="#contact">Contact</a>
+  <a href="https://www.linkedin.com/in/kgotso-maduna-24a4641aa/">Profile</a>
+
+ 
+</div>
+
+
+
+
+
+
+
+<div class= "content-wrapper">
+					<div class ="portfolio-items-wrapper">
+						<div class ="portfolio-item-wrapper">
+							<div class ="portfolio-img-background" style="background-image:url(Assets/Images/img2.jpg"></div>
+						<h1 style="margin-left: 5%;"><marquee behavior="slide" direction="down">Hey, I am Kgotso :). A Software Development student at Tshwane University of Technology.</h1></marquee>
+						<h3 style="margin-left: 40%;">Welcome to my page!</h3>
+						
+						
+			
+            <!--<img src="Assets/Images/firstImage.jpg"  >-->
+
+			<div class= "logo-wrapper">
+						<img src= "Assets/Images/logo.jpg" 
+						class = "myLogo" alt=" style= width: 20%;
+						
+				   
+						" style="width: 30%;margin-left: 35%;opacity:0.7;height:60%;"
+					 >
+
+
+					 
+              
+
+                     </div>
+               </div>
+
+
+                      </div>
+    
+
+</div>	
+
+
+<div class="about" id="bout" style="height:600px;">
+ <div class="contact"  id="contactt"  style= "height: 1700px";>
+
+
+	
+</div>	
+<div class="about" id="bout" style="height:600px;">
+
+
+
+
+</div>
+
+
+ 
+</div>	
+
+		
+<div class="about" id="About" style="height:900px">
+
+
+    <div>
+
+	</div>
+	<p >I am an aspiring Software Developer, who is willing to learn more about the Computer Science industry. I am currently learning towards a National Diploma in Information Technology, Software Development at Tshwane University of Technology. In addition to what was already mentioned, I can describe myself as an initiative-taking individual with passion for developing software. My passion goes far more beyond working during office hours. I understand very well that a career in Tech is more about continous learning and is being willing to sacrifice my time to encourage my growth in the Tech world.</p>
+
+	<p>I now am able to use languges like c++, and java and can work with web technologies such as HTML, CSS, as well as some Javascript. Currently I am learning how to use libraries such as Codeigniter, Laravel as well as react js. I am also learning how to use the MVC framework, C#, and how to use APIs.</p>
+
+    <p>In the future, I would like to know how to use Angular, Rasor Views, Entity Framework, and many more. I also would like to be able to be an Android and IOS Developer. It would give me great joy to see myself being able to create real stuff like developing fast loading, accessable and easy to use websites. I am Kgotso and that is my story, Thanks! </p>
+</div>	
+
+<div class="contact" id="contact" style="height:1700px">
+<h1 style="margin-left: 560px;width: 600px;">Contact Me</h1>
+<h3 style="margin-left: 500px;width:600px">Cellphone Number - 0728808927</h3>
+<h3 style="margin-left: 500px;width:600px">Email Address - kgmaduna01@gmail.com</h3>
+
+
+
+
+
+</body>
+</html>
